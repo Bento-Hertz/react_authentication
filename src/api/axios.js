3724dcfa49dd1ef863c33fc63160795fdee46881
Bento-Hertz <bentoluiz50@gmail.com>
2024-05-17 11:22:48 -0300

    adding login authentication using context api

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,4 +1,4 @@
-import axios from '../../reference/api/axios';
+import axios from 'axios';
 
 export default axios.create({
     baseURL: 'http://localhost:3500'
--- src/index.js
+++ src/index.js
@@ -2,10 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { AuthProvider } from '../reference/login/context/AuthProvider';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
+  <React.StrictMode>]
+    <AuthProvider>
+      <App />
+    </AuthProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
